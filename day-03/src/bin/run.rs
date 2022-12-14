02155d8ff4dbc61ea3968b8d066d18ed36457bf5
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-12-14 16:58:43 +0100

    Day 3 p1

---
+++ day-03/src/bin/run.rs
@@ -0,0 +1,8 @@
+use day_03::{run_p1, run_p2};
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("./input.txt").unwrap();
+    println!("p1: {}", run_p1(&file));
+    println!("p2: {}", run_p2(&file));
+}
